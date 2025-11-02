25ee8f735b8951b0511760884d433f726a2656bf
valeria <val@gmail.com>
2025-11-02 02:25:54 -0300

    versão 2.0

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -26,7 +26,7 @@ export default function HomeScreen() {
           suas informações pessoais.
         </Text>
       </View>
-      <View style={GlobalStyles.card}>
+      <View style={{ backgroundColor: "#fff", padding: 20, borderRadius: 10, marginTop: 0, alignItems: 'center' }}>
         <Image
           source={{
             uri: "https://static.vecteezy.com/ti/vetor-gratis/p1/48972540-rosa-do-utilizador-icone-arredondado-perfil-icone-vetor.jpg",
@@ -34,12 +34,18 @@ export default function HomeScreen() {
           style={{ width: 150, height: 150, alignSelf: "center" }}
         />
         <Text>
-          <Text style={GlobalStyles.label}>
-          O que é o PerfilApp? Imagine que você tem uma ficha cadastral digital sempre à mão,
+          <Text style={GlobalStyles.title}>Bem-vindo ao App de Perfil</Text>
+      <View style={[GlobalStyles.card, { paddingBottom: 5}]}>
+        <Text
+          style={[
+            { textAlign: "center", color: "#DC143C", fontSize:9 },
+          ]}
+        >
+           O que é o PerfilApp? Imagine que você tem uma ficha cadastral digital sempre à mão,
           no seu celular. Ele é como uma carteira digital onde você guarda:
-          </Text>
-
-          <View>
+        </Text>
+        </View>
+          <View style={{ paddingTop: 10 }}>
             <Text style={GlobalStyles.text}>
               • Nome
             </Text>
